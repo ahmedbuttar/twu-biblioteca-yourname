6e47e6f39e9b08433db0c9796fef70c884e2d46e
Ahmed Buttar <ahmedbuttar@Ahmeds-MacBook-Pro.local>
2019-06-09 18:07:22 -0500

    card 3 implemented

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,30 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         showMessage();
+        showBooks();
     }
+
 
     public static void showMessage(){
         System.out.println("Welcome to Biblioteca, Your one-stop-shop for great book titles in Bangalore!");
+    }
+    public static void showBooks(){
+        Book book = new Book("Algebra 101","Tom",1997);
+        Book book1 = new Book("Java 101","Arthur",2001);
+        Book book2 = new Book("Python 101","Jim",2004);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(book);
+        books.add(book1);
+        books.add(book2);
+
+        for(int i = 0;i<books.size();i++){
+            System.out.println(books.get(i).getName() + ", "+ books.get(i).getAuthor()+ ", "+books.get(i).getPublished());
+        }
+
     }
 }
--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,4 +1,33 @@
 package com.twu.biblioteca;
 
 public class Book {
+    private String name;
+    private String author;
+    private int published;
+
+    Book(String name,String author,int published){
+        this.name = name;
+        this.author = author;
+        this.published = published;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public void setAuthor(){
+        this.author = author;
+    }
+    public int getPublished() {
+        return published;
+    }
+    public void setPublished(int published) {
+        this.published = published;
+    }
 }
